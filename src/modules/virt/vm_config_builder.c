61a855137f57aba77d26578e6f60de8f909fe5bd
hardcoremonk <smtlkbs0312@gmail.com>
2026-02-18 21:55:51 +0900

    feat(virt): Stateless VM 설정 빌더 구현 및 ZFS 호환성 패치
    - 빌더 리팩토링: XML 문자열 대신 GVirConfigDomain 객체를 반환하도록 변경 (메모리 관리 개선)
    - ZFS 호환성: ZVol(/dev/zvol/...) 인식을 위해 디스크 타입을 FILE에서 BLOCK으로 수정
    - API 최신화: Deprecated된 드라이버 설정 API를 GVirConfigDomainDiskDriver로 교체

--- src/modules/virt/vm_config_builder.c
+++ src/modules/virt/vm_config_builder.c
@@ -1,195 +1,120 @@
-/* src/modules/virt/vm_config_builder.c */
 #include "vm_config_builder.h"
 #include <libvirt-gconfig/libvirt-gconfig.h>
-#include <string.h>
+#include <gio/gio.h>
 
-/* Private Structure */
-struct _PureCVisorVmConfigBuilder {
-    GObject parent_instance;
-    PureCVisorVmConfig *config;
-};
-
-G_DEFINE_TYPE(PureCVisorVmConfigBuilder, purecvisor_vm_config_builder, G_TYPE_OBJECT)
-
-static void
-purecvisor_vm_config_builder_dispose(GObject *object)
-{
-    PureCVisorVmConfigBuilder *self = PURECVISOR_VM_CONFIG_BUILDER(object);
-
-    if (self->config) {
-        purecvisor_vm_config_free(self->config);
-        self->config = NULL;
-    }
-
-    G_OBJECT_CLASS(purecvisor_vm_config_builder_parent_class)->dispose(object);
-}
-
-static void
-purecvisor_vm_config_builder_class_init(PureCVisorVmConfigBuilderClass *klass)
-{
-    GObjectClass *object_class = G_OBJECT_CLASS(klass);
-    object_class->dispose = purecvisor_vm_config_builder_dispose;
-}
-
-static void
-purecvisor_vm_config_builder_init(PureCVisorVmConfigBuilder *self)
-{
-    self->config = NULL;
-}
-
-PureCVisorVmConfigBuilder *
-purecvisor_vm_config_builder_new(void)
-{
-    return g_object_new(PURECVISOR_TYPE_VM_CONFIG_BUILDER, NULL);
-}
-
-void
-purecvisor_vm_config_free(PureCVisorVmConfig *config)
-{
-    if (!config) return;
-    g_free(config->name);
-    g_free(config->arch);
-    g_free(config->os_type);
-    g_free(config->disk_path);
-    g_free(config->iso_path);
-    g_free(config->bridge_iface);
-    g_free(config);
-}
-
-gboolean
-purecvisor_vm_config_builder_set_config(PureCVisorVmConfigBuilder *self,
-                                        PureCVisorVmConfig *config,
-                                        GError **error)
-{
-    g_return_val_if_fail(PURECVISOR_IS_VM_CONFIG_BUILDER(self), FALSE);
-    g_return_val_if_fail(config != NULL, FALSE);
-
-    if (!config->name || strlen(config->name) == 0) {
-        g_set_error(error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT, "VM name is required");
-        return FALSE;
-    }
-
-    if (self->config) {
-        purecvisor_vm_config_free(self->config);
-    }
-
-    self->config = g_new0(PureCVisorVmConfig, 1);
-    self->config->name = g_strdup(config->name);
-    self->config->memory_kb = config->memory_kb;
-    self->config->vcpus = config->vcpus;
-    self->config->arch = config->arch ? g_strdup(config->arch) : g_strdup("x86_64");
-    self->config->os_type = config->os_type ? g_strdup(config->os_type) : g_strdup("hvm");
-    self->config->disk_path = g_strdup(config->disk_path);
-    self->config->iso_path = g_strdup(config->iso_path);
-    self->config->bridge_iface = config->bridge_iface ? g_strdup(config->bridge_iface) : g_strdup("virbr0");
-
-    return TRUE;
-}
-
-static void
-_build_os_section(GVirConfigDomain *domain, PureCVisorVmConfig *config)
-{
+/* ... _build_os_section, _build_cpu_mem 함수는 기존과 동일 ... */
+static void _build_os_section(GVirConfigDomain *domain, PureCVisorVmConfig *config) {
     GVirConfigDomainOs *os = gvir_config_domain_os_new();
-    
     gvir_config_domain_os_set_os_type(os, GVIR_CONFIG_DOMAIN_OS_TYPE_HVM);
-    gvir_config_domain_os_set_arch(os, config->arch);
+    gvir_config_domain_os_set_arch(os, "x86_64");
     
+    GList *boot_devs = NULL;
+    boot_devs = g_list_append(boot_devs, (gpointer)GVIR_CONFIG_DOMAIN_OS_BOOT_DEVICE_CDROM);
+    boot_devs = g_list_append(boot_devs, (gpointer)GVIR_CONFIG_DOMAIN_OS_BOOT_DEVICE_HD);
+    gvir_config_domain_os_set_boot_devices(os, boot_devs);
+    g_list_free(boot_devs);
+
     gvir_config_domain_set_os(domain, os);
     g_object_unref(os);
 }
 
-static void
-_set_features_list(GVirConfigDomain *domain)
-{
-    const gchar *features[] = { "acpi", "apic", NULL };
-    gvir_config_domain_set_features(domain, (GStrv)features);
+static void _build_cpu_mem(GVirConfigDomain *domain, PureCVisorVmConfig *config) {
+    gvir_config_domain_set_memory(domain, config->memory_mb * 1024);
+    gvir_config_domain_set_vcpus(domain, config->vcpu);
+    
+    GVirConfigDomainCpu *cpu = gvir_config_domain_cpu_new();
+    gvir_config_domain_cpu_set_mode(cpu, GVIR_CONFIG_DOMAIN_CPU_MODE_HOST_MODEL);
+    gvir_config_domain_set_cpu(domain, cpu);
+    g_object_unref(cpu);
 }
 
-static void
-_build_disk_device(GVirConfigDomain *domain, PureCVisorVmConfig *config)
-{
-    if (!config->disk_path) return;
-
-    GVirConfigDomainDisk *disk = gvir_config_domain_disk_new();
-    gvir_config_domain_disk_set_type(disk, GVIR_CONFIG_DOMAIN_DISK_BLOCK);
-    gvir_config_domain_disk_set_guest_device_type(disk, GVIR_CONFIG_DOMAIN_DISK_GUEST_DEVICE_DISK);
-    gvir_config_domain_disk_set_source(disk, config->disk_path);
-    
+/* [Helper] Disk Driver 설정 */
+static void _set_disk_driver(GVirConfigDomainDisk *disk, const gchar *name, GVirConfigDomainDiskFormat format) {
     GVirConfigDomainDiskDriver *driver = gvir_config_domain_disk_driver_new();
-    gvir_config_domain_disk_driver_set_name(driver, "qemu");
-    gvir_config_domain_disk_driver_set_format(driver, GVIR_CONFIG_DOMAIN_DISK_FORMAT_RAW);
-    gvir_config_domain_disk_driver_set_cache(driver, GVIR_CONFIG_DOMAIN_DISK_CACHE_NONE);
-    
+    gvir_config_domain_disk_driver_set_name(driver, name);
+    gvir_config_domain_disk_driver_set_format(driver, format);
     gvir_config_domain_disk_set_driver(disk, driver);
-    gvir_config_domain_disk_set_target_bus(disk, GVIR_CONFIG_DOMAIN_DISK_BUS_VIRTIO);
-    gvir_config_domain_disk_set_target_dev(disk, "vda");
-
-    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(disk));
-    
-    g_object_unref(disk);
     g_object_unref(driver);
 }
 
-static void
-_build_net_interface(GVirConfigDomain *domain, PureCVisorVmConfig *config)
-{
-    GVirConfigDomainInterfaceBridge *iface = gvir_config_domain_interface_bridge_new();
-    GVirConfigDomainInterfaceBridge *bridge_source = GVIR_CONFIG_DOMAIN_INTERFACE_BRIDGE(iface);
-    gvir_config_domain_interface_bridge_set_source(bridge_source, config->bridge_iface);
-    gvir_config_domain_interface_set_model(GVIR_CONFIG_DOMAIN_INTERFACE(iface), "virtio");
+static void _build_disk_device(GVirConfigDomain *domain, PureCVisorVmConfig *config) {
+    /* 1. Main Disk (ZFS Volume) -> BLOCK DEVICE */
+    gchar *zvol_path = g_strdup_printf("/dev/zvol/tank/%s", config->name);
+    
+    GVirConfigDomainDisk *disk = gvir_config_domain_disk_new();
+    
+    // [CRITICAL FIX] ZVol은 블록 장치이므로 GVIR_CONFIG_DOMAIN_DISK_BLOCK 사용
+    gvir_config_domain_disk_set_type(disk, GVIR_CONFIG_DOMAIN_DISK_BLOCK);
+    
+    gvir_config_domain_disk_set_guest_device_type(disk, GVIR_CONFIG_DOMAIN_DISK_GUEST_DEVICE_DISK);
+    
+    // Driver: qemu, Format: raw
+    _set_disk_driver(disk, "qemu", GVIR_CONFIG_DOMAIN_DISK_FORMAT_RAW);
+    
+    gvir_config_domain_disk_set_source(disk, zvol_path);
+    gvir_config_domain_disk_set_target_bus(disk, GVIR_CONFIG_DOMAIN_DISK_BUS_VIRTIO);
+    gvir_config_domain_disk_set_target_dev(disk, "vda");
+    
+    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(disk));
+    g_free(zvol_path);
+    g_object_unref(disk);
 
-    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(iface));
-    g_object_unref(iface);
+    /* 2. CD-ROM (ISO) -> FILE DEVICE */
+    if (config->iso_path && g_strcmp0(config->iso_path, "") != 0) {
+        GVirConfigDomainDisk *cdrom = gvir_config_domain_disk_new();
+        
+        // [NOTE] ISO 파일은 일반 파일이므로 GVIR_CONFIG_DOMAIN_DISK_FILE 유지
+        gvir_config_domain_disk_set_type(cdrom, GVIR_CONFIG_DOMAIN_DISK_FILE);
+        
+        gvir_config_domain_disk_set_guest_device_type(cdrom, GVIR_CONFIG_DOMAIN_DISK_GUEST_DEVICE_CDROM);
+        
+        _set_disk_driver(cdrom, "qemu", GVIR_CONFIG_DOMAIN_DISK_FORMAT_RAW);
+        
+        gvir_config_domain_disk_set_source(cdrom, config->iso_path);
+        gvir_config_domain_disk_set_target_bus(cdrom, GVIR_CONFIG_DOMAIN_DISK_BUS_SATA);
+        gvir_config_domain_disk_set_target_dev(cdrom, "hda");
+        gvir_config_domain_disk_set_readonly(cdrom, TRUE);
+
+        gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(cdrom));
+        g_object_unref(cdrom);
+    }
 }
 
-/* [CRITICAL FIX] Includes 'kvm' type setting */
-gchar *
-purecvisor_vm_config_builder_generate_xml(PureCVisorVmConfigBuilder *self,
-                                          GError **error)
-{
-    g_return_val_if_fail(PURECVISOR_IS_VM_CONFIG_BUILDER(self), NULL);
+static void _build_net_interface(GVirConfigDomain *domain, PureCVisorVmConfig *config) {
+    GVirConfigDomainInterfaceNetwork *net = gvir_config_domain_interface_network_new();
+    gvir_config_domain_interface_network_set_source(net, "default");
+    gvir_config_domain_interface_set_model(GVIR_CONFIG_DOMAIN_INTERFACE(net), "virtio");
+    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(net));
+    g_object_unref(net);
+}
 
-    if (!self->config) {
-        g_set_error(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Configuration not set");
+static void _build_graphics(GVirConfigDomain *domain, PureCVisorVmConfig *config) {
+    GVirConfigDomainGraphicsVnc *vnc = gvir_config_domain_graphics_vnc_new();
+    gvir_config_domain_graphics_vnc_set_autoport(vnc, TRUE);
+    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(vnc));
+    g_object_unref(vnc);
+    
+    GVirConfigDomainVideo *video = gvir_config_domain_video_new();
+    gvir_config_domain_video_set_model(video, GVIR_CONFIG_DOMAIN_VIDEO_MODEL_QXL);
+    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(video));
+    g_object_unref(video);
+}
+
+GVirConfigDomain *purecvisor_vm_config_builder_create_config(PureCVisorVmConfig *config, GError **error) {
+    if (!config || !config->name) {
+        g_set_error(error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT, "Invalid VM Configuration");
         return NULL;
     }
 
     GVirConfigDomain *domain = gvir_config_domain_new();
-    
-    /* [IMPORTANT] KVM Type Set */
+    gvir_config_domain_set_name(domain, config->name);
     gvir_config_domain_set_virt_type(domain, GVIR_CONFIG_DOMAIN_VIRT_KVM);
-
-    gvir_config_domain_set_name(domain, self->config->name);
-    gvir_config_domain_set_memory(domain, self->config->memory_kb);
-    gvir_config_domain_set_vcpus(domain, self->config->vcpus);
     
-    gvir_config_domain_set_lifecycle(domain, 
-        GVIR_CONFIG_DOMAIN_LIFECYCLE_ON_POWEROFF, GVIR_CONFIG_DOMAIN_LIFECYCLE_DESTROY);
-    gvir_config_domain_set_lifecycle(domain, 
-        GVIR_CONFIG_DOMAIN_LIFECYCLE_ON_REBOOT, GVIR_CONFIG_DOMAIN_LIFECYCLE_RESTART);
-    gvir_config_domain_set_lifecycle(domain, 
-        GVIR_CONFIG_DOMAIN_LIFECYCLE_ON_CRASH, GVIR_CONFIG_DOMAIN_LIFECYCLE_RESTART);
+    _build_os_section(domain, config);
+    _build_cpu_mem(domain, config);
+    _build_disk_device(domain, config);
+    _build_net_interface(domain, config);
+    _build_graphics(domain, config);
 
-    _build_os_section(domain, self->config);
-    _set_features_list(domain); 
-
-    _build_disk_device(domain, self->config);
-    _build_net_interface(domain, self->config);
-    
-    GVirConfigDomainGraphicsVnc *vnc = gvir_config_domain_graphics_vnc_new();
-    gvir_config_domain_graphics_vnc_set_autoport(vnc, TRUE);
-    gvir_config_domain_graphics_vnc_set_port(vnc, -1);
-    gvir_config_domain_add_device(domain, GVIR_CONFIG_DOMAIN_DEVICE(vnc));
-    g_object_unref(vnc);
-
-    gchar *xml_str = gvir_config_object_to_xml(GVIR_CONFIG_OBJECT(domain));
-    
-    g_object_unref(domain);
-
-    if (!xml_str) {
-         g_set_error(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Failed to serialize XML");
-         return NULL;
-    }
-
-    return xml_str;
+    return domain;
 }
--- src/modules/virt/vm_config_builder.h
+++ src/modules/virt/vm_config_builder.h
@@ -1,70 +1,17 @@
-/*
- * src/modules/virt/vm_config_builder.h
- *
- * Description:
- * Converts generic VM configuration parameters into Libvirt-compatible XML
- * using libvirt-gconfig. Strictly synchronous and purely functional.
- *
- * Author: PureCVisor Architect
- */
-
-#ifndef PURECVISOR_VM_CONFIG_BUILDER_H
-#define PURECVISOR_VM_CONFIG_BUILDER_H
+#pragma once
 
 #include <glib-object.h>
 #include <libvirt-gconfig/libvirt-gconfig.h>
-#include <json-glib/json-glib.h>
+#include "vm_types.h" // 공통 타입 포함
 
 G_BEGIN_DECLS
 
-/* Type Declaration */
-#define PURECVISOR_TYPE_VM_CONFIG_BUILDER (purecvisor_vm_config_builder_get_type())
-
-G_DECLARE_FINAL_TYPE(PureCVisorVmConfigBuilder, purecvisor_vm_config_builder, PURECVISOR, VM_CONFIG_BUILDER, GObject)
+// 구조체 정의 제거됨 (vm_types.h로 이동)
 
 /**
- * PureCVisorVmConfig:
- * A plain struct to hold parsed parameters before building XML.
- * This separates JSON parsing logic from XML building logic.
+ * VM 설정을 담은 GVirConfigDomain 객체를 생성하여 반환합니다.
+ * Caller는 반환된 객체를 g_object_unref() 해야 합니다.
  */
-typedef struct {
-    gchar *name;
-    guint64 memory_kb;
-    guint vcpus;
-    gchar *arch;        // e.g., "x86_64"
-    gchar *os_type;     // e.g., "hvm"
-    
-    /* Storage Configuration */
-    gchar *disk_path;   // Path to the ZVol (e.g., /dev/zvol/tank/vm-100)
-    gchar *iso_path;    // Optional CD-ROM ISO path
-    
-    /* Network Configuration */
-    gchar *bridge_iface; // e.g., "br0"
-} PureCVisorVmConfig;
-
-/* Constructor */
-PureCVisorVmConfigBuilder *purecvisor_vm_config_builder_new(void);
-
-/*
- * Method: purecvisor_vm_config_builder_set_config
- * Description: Validates and sets the configuration data.
- */
-gboolean purecvisor_vm_config_builder_set_config(PureCVisorVmConfigBuilder *self,
-                                                 PureCVisorVmConfig *config,
-                                                 GError **error);
-
-/*
- * Method: purecvisor_vm_config_builder_generate_xml
- * Description:
- * Constructs the GVirConfigDomain hierarchy and returns the XML string.
- * The caller owns the returned string.
- */
-gchar *purecvisor_vm_config_builder_generate_xml(PureCVisorVmConfigBuilder *self,
-                                                 GError **error);
-
-/* Helper to free the plain config struct */
-void purecvisor_vm_config_free(PureCVisorVmConfig *config);
+GVirConfigDomain *purecvisor_vm_config_builder_create_config(PureCVisorVmConfig *config, GError **error);
 
 G_END_DECLS
-
-#endif /* PURECVISOR_VM_CONFIG_BUILDER_H */
