69714c4dcb60bb1f3e44858a244e44722ddbdb29
hardcoremonk <smtlkbs0312@gmail.com>
2026-02-19 19:33:45 +0900

    refactor(storage): ZFS 드라이버 구조 단순화 및 안정성 강화
    - 복잡한 GObject 래퍼를 제거하고 동기식 헬퍼 함수로 변경
    - 'zfs create/destroy' 실행을 위한 CLI 실행 래퍼 구현
    - G_IO_ERROR 처리를 추가하여 에러 리포팅 메커니즘 개선

--- src/modules/storage/zfs_driver.c
+++ src/modules/storage/zfs_driver.c
@@ -1,211 +1,46 @@
-/*
- * src/modules/storage/zfs_driver.c
- *
- * Description:
- * Implementation of Non-blocking ZFS Driver.
- * Updated for Phase 3.
- */
+/* src/modules/storage/zfs_driver.c */
 
 #include "zfs_driver.h"
+#include <glib.h>
+#include <gio/gio.h>    // [Fix] G_IO_ERROR 사용을 위해 필수
 #include <stdio.h>
 
-struct _PureCVisorZfsDriver {
-    GObject parent_instance;
-};
-
-G_DEFINE_TYPE(PureCVisorZfsDriver, purecvisor_zfs_driver, G_TYPE_OBJECT)
-
-static void
-purecvisor_zfs_driver_init(PureCVisorZfsDriver *self)
-{
-    (void)self; /* Unused */
-}
-
-static void
-purecvisor_zfs_driver_class_init(PureCVisorZfsDriverClass *klass)
-{
-    (void)klass; /* Unused */
-}
-
-PureCVisorZfsDriver *
-purecvisor_zfs_driver_new(void)
-{
-    return g_object_new(PURECVISOR_TYPE_ZFS_DRIVER, NULL);
-}
-
-static gboolean
-_zfs_subprocess_finish_helper(GAsyncResult *res, GError **error)
-{
-    GSubprocess *proc = NULL;
-    gboolean success = FALSE;
-    GError *local_error = NULL;
-    GBytes *stderr_bytes = NULL; /* FIX: Changed from gchar* to GBytes* */
-
-    proc = G_SUBPROCESS(g_async_result_get_source_object(res));
-
-    /* Finish communication */
-    /* Note: We pass NULL for stdout_bytes because we don't need it here */
-    if (!g_subprocess_communicate_finish(proc, res, NULL, &stderr_bytes, &local_error)) {
-        g_propagate_error(error, local_error);
-        if (stderr_bytes) g_bytes_unref(stderr_bytes);
+// Helper to run command
+static gboolean _run_command(const gchar *cmd, GError **error) {
+    gint exit_status;
+    gchar *std_err = NULL;
+    GError *spawn_err = NULL;
+    
+    // g_spawn_command_line_sync is blocking but safe for simple CLI tools in this phase
+    if (!g_spawn_command_line_sync(cmd, NULL, &std_err, &exit_status, &spawn_err)) {
+        g_propagate_error(error, spawn_err);
+        g_free(std_err);
         return FALSE;
     }
 
-    /* Check Exit Status */
-    if (g_subprocess_get_if_exited(proc) && g_subprocess_get_successful(proc)) {
-        success = TRUE;
-    } else {
-        /* Convert GBytes to string for error message */
-        gchar *err_str = NULL;
-        if (stderr_bytes) {
-            gsize len;
-            const gchar *data = g_bytes_get_data(stderr_bytes, &len);
-            if (len > 0) {
-                err_str = g_strndup(data, len); /* Ensure null-termination */
-            }
-        }
-
-        gchar *clean_err = (err_str && *err_str) ? g_strstrip(err_str) : "Unknown ZFS error";
+    if (exit_status != 0) {
         g_set_error(error, G_IO_ERROR, G_IO_ERROR_FAILED, 
-                    "ZFS Command Failed: %s", clean_err);
-        
-        g_free(err_str);
+                    "ZFS Command Failed: %s (Stderr: %s)", cmd, std_err);
+        g_free(std_err);
+        return FALSE;
     }
 
-    /* Cleanup GBytes object */
-    if (stderr_bytes) {
-        g_bytes_unref(stderr_bytes);
-    }
-
-    return success;
+    g_free(std_err);
+    return TRUE;
 }
 
-
-
-static void
-_on_create_vol_communicate_complete(GObject *source, GAsyncResult *res, gpointer user_data)
-{
-    (void)source;
-    GTask *task = G_TASK(user_data);
-    GError *error = NULL;
-
-    if (_zfs_subprocess_finish_helper(res, &error)) {
-        gchar *full_path = g_strdup((gchar *)g_task_get_task_data(task));
-        g_task_return_pointer(task, full_path, g_free);
-    } else {
-        g_task_return_error(task, error);
-    }
-
-    g_object_unref(task);
+gboolean purecvisor_zfs_create_volume(const gchar *pool, const gchar *name, const gchar *size, GError **error) {
+    // Command: zfs create -V <size> <pool>/<name>
+    gchar *cmd = g_strdup_printf("zfs create -V %s %s/%s", size, pool, name);
+    gboolean ret = _run_command(cmd, error);
+    g_free(cmd);
+    return ret;
 }
 
-void
-purecvisor_zfs_driver_create_vol_async(PureCVisorZfsDriver *self,
-                                       const gchar *pool_name,
-                                       const gchar *vol_name,
-                                       guint64 size_bytes,
-                                       GCancellable *cancellable,
-                                       GAsyncReadyCallback callback,
-                                       gpointer user_data)
-{
-    g_return_if_fail(PURECVISOR_IS_ZFS_DRIVER(self));
-
-    GTask *task = g_task_new(self, cancellable, callback, user_data);
-    GError *error = NULL;
-
-    gchar *zvol_path_arg = g_strdup_printf("%s/%s", pool_name, vol_name);
-    gchar *size_str = g_strdup_printf("%lu", size_bytes);
-    gchar *dev_path = g_strdup_printf("/dev/zvol/%s/%s", pool_name, vol_name);
-    
-    g_task_set_task_data(task, dev_path, g_free);
-
-    GSubprocess *proc = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE | G_SUBPROCESS_FLAGS_STDERR_PIPE,
-                                         &error,
-                                         "zfs", "create", "-V", size_str, "-p", zvol_path_arg, NULL);
-
-    g_free(zvol_path_arg);
-    g_free(size_str);
-
-    if (!proc) {
-        g_task_return_error(task, error);
-        g_object_unref(task);
-        return;
-    }
-
-    g_subprocess_communicate_async(proc, NULL, cancellable, 
-                                   _on_create_vol_communicate_complete, task);
-    g_object_unref(proc);
-}
-
-gboolean
-purecvisor_zfs_driver_create_vol_finish(PureCVisorZfsDriver *self,
-                                        GAsyncResult *res,
-                                        gchar **out_full_path,
-                                        GError **error)
-{
-    g_return_val_if_fail(g_task_is_valid(res, self), FALSE);
-
-    gchar *path = g_task_propagate_pointer(G_TASK(res), error);
-    if (path) {
-        if (out_full_path) *out_full_path = path;
-        else g_free(path);
-        return TRUE;
-    }
-    return FALSE;
-}
-
-static void
-_on_destroy_vol_communicate_complete(GObject *source, GAsyncResult *res, gpointer user_data)
-{
-    (void)source;
-    GTask *task = G_TASK(user_data);
-    GError *error = NULL;
-
-    if (_zfs_subprocess_finish_helper(res, &error)) {
-        g_task_return_boolean(task, TRUE);
-    } else {
-        g_task_return_error(task, error);
-    }
-    g_object_unref(task);
-}
-
-void
-purecvisor_zfs_driver_destroy_vol_async(PureCVisorZfsDriver *self,
-                                        const gchar *pool_name,
-                                        const gchar *vol_name,
-                                        GCancellable *cancellable,
-                                        GAsyncReadyCallback callback,
-                                        gpointer user_data)
-{
-    g_return_if_fail(PURECVISOR_IS_ZFS_DRIVER(self));
-
-    GTask *task = g_task_new(self, cancellable, callback, user_data);
-    GError *error = NULL;
-
-    gchar *zvol_path_arg = g_strdup_printf("%s/%s", pool_name, vol_name);
-
-    GSubprocess *proc = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE | G_SUBPROCESS_FLAGS_STDERR_PIPE,
-                                         &error,
-                                         "zfs", "destroy", "-r", zvol_path_arg, NULL);
-
-    g_free(zvol_path_arg);
-
-    if (!proc) {
-        g_task_return_error(task, error);
-        g_object_unref(task);
-        return;
-    }
-
-    g_subprocess_communicate_async(proc, NULL, cancellable, 
-                                   _on_destroy_vol_communicate_complete, task);
-    g_object_unref(proc);
-}
-
-gboolean
-purecvisor_zfs_driver_destroy_vol_finish(PureCVisorZfsDriver *self,
-                                         GAsyncResult *res,
-                                         GError **error)
-{
-    g_return_val_if_fail(g_task_is_valid(res, self), FALSE);
-    return g_task_propagate_boolean(G_TASK(res), error);
+gboolean purecvisor_zfs_destroy_volume(const gchar *pool, const gchar *name, GError **error) {
+    // Command: zfs destroy <pool>/<name>
+    gchar *cmd = g_strdup_printf("zfs destroy %s/%s", pool, name);
+    gboolean ret = _run_command(cmd, error);
+    g_free(cmd);
+    return ret;
 }
