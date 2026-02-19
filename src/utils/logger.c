07f9206f9f886d30e943ee2e087814664c9f5311
hardcoremonk <smtlkbs0312@gmail.com>
2026-02-19 12:38:34 +0900

    공통 로깅 모듈(src/utils/logger) 추가 및 초기화 로직 적용

--- src/utils/logger.c
+++ src/utils/logger.c
@@ -1,8 +1,50 @@
 #include "logger.h"
 #include <stdio.h>
+#include <time.h>
+
+// 간단한 로그 핸들러: 시간 + 레벨 + 메시지 출력
+static void _log_handler(const gchar *log_domain,
+                         GLogLevelFlags log_level,
+                         const gchar *message,
+                         gpointer user_data) {
+    time_t now;
+    struct tm *tm_info;
+    char time_buf[20];
+    const char *level_str = "INFO";
+    const char *color = "\033[0m"; // Reset
+
+    time(&now);
+    tm_info = localtime(&now);
+    strftime(time_buf, 20, "%H:%M:%S", tm_info);
+
+    if (log_level & G_LOG_LEVEL_ERROR) {
+        level_str = "ERROR";
+        color = "\033[1;31m"; // Red
+    } else if (log_level & G_LOG_LEVEL_CRITICAL) {
+        level_str = "CRIT";
+        color = "\033[1;35m"; // Magenta
+    } else if (log_level & G_LOG_LEVEL_WARNING) {
+        level_str = "WARN";
+        color = "\033[1;33m"; // Yellow
+    } else if (log_level & G_LOG_LEVEL_MESSAGE) {
+        level_str = "MSG";
+        color = "\033[1;32m"; // Green
+    } else if (log_level & G_LOG_LEVEL_INFO) {
+        level_str = "INFO";
+        color = "\033[1;34m"; // Blue
+    } else if (log_level & G_LOG_LEVEL_DEBUG) {
+        level_str = "DEBUG";
+        color = "\033[1;30m"; // Gray
+    }
+
+    fprintf(stdout, "%s[%s] %s%s: %s\033[0m\n",
+            color, time_buf, level_str,
+            log_domain ? log_domain : "",
+            message);
+    fflush(stdout);
+}
 
 void purecvisor_logger_init(void) {
-    // GLib 기본 로거 사용 (stdout/stderr)
-    // 필요 시 파일 로깅 등을 여기에 추가
-    g_setenv("G_MESSAGES_DEBUG", "all", TRUE); 
+    // GLib의 기본 로그 핸들러를 커스텀 핸들러로 교체
+    g_log_set_default_handler(_log_handler, NULL);
 }
--- src/utils/logger.h
+++ src/utils/logger.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef PURECVISOR_LOGGER_H
+#define PURECVISOR_LOGGER_H
 
 #include <glib.h>
 
 void purecvisor_logger_init(void);
+
+#endif // PURECVISOR_LOGGER_H
